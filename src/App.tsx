7ede5ec4565de143405d329b2fe4af2c290f79d8
EduNunesGithub <edu.nunes.github@gmail.com>
2025-01-11 14:48:33 -0300

    feat(component): vehicle description

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { clsx } from "clsx";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { LegalText } from "./components/LegalText/LegalText";
 import { Vehicle } from "./components/Vehicle/Vehicle";
+import { VehicleDescription } from "./components/VehicleDescription/VehicleDescription";
 import styles from "./App.module.less";
 
 const App = () => (
@@ -15,11 +17,32 @@ const App = () => (
         </div>
       </section>
 
-      <section className={styles["legal-text-section"]}>
-        <div className={styles["legal-text-section__container"]}>
-          <LegalText />
+      <div className={styles["light-background"]}>
+        <div className={styles.divider}>
+          <div
+            className={clsx(
+              styles.divider__line,
+              styles["divider__line--strong"],
+            )}
+          />
         </div>
-      </section>
+
+        <section className={styles["vehicle-description-section"]}>
+          <div className={styles["vehicle-description-section__container"]}>
+            <VehicleDescription />
+          </div>
+        </section>
+
+        <div className={styles.divider}>
+          <div className={styles.divider__line} />
+        </div>
+
+        <section className={styles["legal-text-section"]}>
+          <div className={styles["legal-text-section__container"]}>
+            <LegalText />
+          </div>
+        </section>
+      </div>
     </main>
 
     <Footer />
