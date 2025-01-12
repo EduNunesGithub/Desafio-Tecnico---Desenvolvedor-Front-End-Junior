74ed05d1969608743bff439364be551c27541d7f
EduNunesGithub <edu.nunes.github@gmail.com>
2025-01-12 18:09:37 -0300

    feat(component): add skeleton for missing components

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,38 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
 import { clsx } from "clsx";
+import Skeleton from "react-loading-skeleton";
+import { api } from "../../lib/api/api";
 import styles from "./Breadcrumbs.module.less";
 
-export const Breadcrumbs = () => (
-  <div className={styles.container}>
-    <ul className={styles["links-list"]}>
-      <li className={styles["links-list__list-item"]}>
-        <a
-          className={clsx(
-            styles["links-list__link"],
-            styles["links-list__link--disabled"],
-          )}
-          href="/"
-        >
-          Ofertas
-        </a>
-      </li>
-      <li className={styles["links-list__list-item"]}>→</li>
-      <li className={styles["links-list__list-item"]}>
-        <a
-          className={styles["links-list__link"]}
-          href="/"
-        >
-          Novo Série 1
-        </a>
-      </li>
-    </ul>
+export const Breadcrumbs = () => {
+  const { data, status } = useQuery({
+    queryKey: ["vehicle"],
+    queryFn: api.vehicle,
+  });
 
-    <a
-      className={styles.container__link}
-      href="/"
-    >
-      Voltar para página anterior
-    </a>
+  return (
+    <div className={styles.container}>
+      <ul className={styles["links-list"]}>
+        <li className={styles["links-list__list-item"]}>
+          <a
+            className={clsx(
+              styles["links-list__link"],
+              styles["links-list__link--disabled"],
+            )}
+            href="/"
+          >
+            Ofertas
+          </a>
+        </li>
+        <li className={styles["links-list__list-item"]}>→</li>
+        <li className={styles["links-list__list-item"]}>
+          <a
+            className={styles["links-list__link"]}
+            href="/"
+          >
+            Novo Série 1
+          </a>
+        </li>
+      </ul>
 
-    <h1 className={styles.container__heading}>BMW Série 4 Cabrio</h1>
-  </div>
-);
+      <a
+        className={styles.container__link}
+        href="/"
+      >
+        Voltar para página anterior
+      </a>
+
+      <h1 className={styles.container__heading}>
+        {status === "success" ? data.name : <Skeleton count={0.25} />}
+      </h1>
+    </div>
+  );
+};
--- src/components/VehicleDescription/VehicleDescription.tsx
+++ src/components/VehicleDescription/VehicleDescription.tsx
@@ -1,22 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import Skeleton from "react-loading-skeleton";
+import { api } from "../../lib/api/api";
 import styles from "./VehicleDescription.module.less";
 
-export const VehicleDescription = () => (
-  <article className={styles.article}>
-    <h2 className={styles.article__heading}>
-      A vida é melhor quando compartilhada. BMW Série 4 Cabrio
-    </h2>
+export const VehicleDescription = () => {
+  const { data, status } = useQuery({
+    queryKey: ["vehicle"],
+    queryFn: api.vehicle,
+  });
 
-    <p className={styles.article__paragraph}>
-      A performance irradia de cada ângulo do design inovador do BMW Série 4
-      Cabrio. Começando pela dianteira, que conta com para-choque clássico dos
-      modelos BMW. Ao correr os olhos pelo design, nota-se imediatamente o teto
-      inclinado e a linha do ombro marcante, que equilibra o centro de gravidade
-      do carro. Voltando-se para as laterais, são os robustos arcos de 10 mm e
-      as incomuns rodas de 21 polegadas que chamam a atenção. Ao chegar na
-      traseira do BMW Série 4 Cabrio, a força do design continua surpreendendo.
-      O para-choque com difusão clássica dos modelos BMW, as lâminas horizontais
-      e teto com spoiler ganham um toque a mais de amplitude devido ao vidro
-      traseiro levemente rebaixado.
-    </p>
-  </article>
-);
+  return (
+    <article className={styles.article}>
+      <h2 className={styles.article__heading}>
+        {status === "success" ?
+          data.description.heading
+        : <Skeleton count={2} />}
+      </h2>
+
+      <p className={styles.article__paragraph}>
+        {status === "success" ?
+          data.description.paragraph
+        : <Skeleton count={11} />}
+      </p>
+    </article>
+  );
+};
