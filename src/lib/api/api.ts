3c7accff843b543fbd6bb3ae34dfe76b937db577
EduNunesGithub <edu.nunes.github@gmail.com>
2025-01-15 22:34:24 -0300

    feat(api): now uses rails api

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -3,14 +3,18 @@ export type CompanyFunction = () => Promise<Company>;
 export type VehicleFunction = () => Promise<Vehicle>;
 
 export const company: CompanyFunction = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/company`);
+  const response = await fetch(
+    `${import.meta.env.VITE_API_BASE_URL_RAILS}/companies/1`,
+  );
   const data: Company = await response.json();
 
   return data;
 };
 
 export const vehicle: VehicleFunction = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/vehicle`);
+  const response = await fetch(
+    `${import.meta.env.VITE_API_BASE_URL_RAILS}/vehicles/1`,
+  );
   const data: Vehicle = await response.json();
 
   return data;
